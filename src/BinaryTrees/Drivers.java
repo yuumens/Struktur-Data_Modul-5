4379e994026e73e58f800a62b114dff0e51118b4
YumanYummy <ahmadrizki0209@gmail.com>
2023-05-25 14:15:02 +0700

    Modul 5: Inital

---
+++ src/BinaryTrees/Drivers.java
@@ -0,0 +1,20 @@
+package BinaryTrees;
+
+public class Drivers {
+    public static void main(String[] args) {
+        BinaryTree pohon = new BinaryTree();
+
+        pohon.NewNode(20);
+        pohon.NewNode(2);
+        pohon.NewNode(25);
+        pohon.NewNode(37);
+        pohon.NewNode(16);
+
+        System.out.println("\nPre Order : ");
+        pohon.preOrder(pohon.root);
+        System.out.println("\n\nIn Order : ");
+        pohon.inOrder(pohon.root);
+        System.out.println("\n\nPost Order : ");
+        pohon.postOrder(pohon.root);
+    }
+}
